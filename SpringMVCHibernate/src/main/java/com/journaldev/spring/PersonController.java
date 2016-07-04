cc7880fc0ef135c788b1b792e9c3318ece08807a
jmishra <jugul.mishra@tavant.com>
2016-07-04 19:38:05 +0530

    hghgfhgfhfg

--- SpringMVCHibernate/src/main/java/com/journaldev/spring/PersonController.java
+++ SpringMVCHibernate/src/main/java/com/journaldev/spring/PersonController.java
@@ -21,7 +21,7 @@ public class PersonController {
 	{
 		System.out.println("inside controller");
 	}
-	// commented fro testing jugal fghhfhfhgh
+	// commented fro testing jugal fghhfhfhgh 
 	@Autowired(required=true)
 	@Qualifier(value="personService")
 	public void setPersonService(PersonService ps){
